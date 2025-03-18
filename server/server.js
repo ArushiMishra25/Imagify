694ba1913085f329bb0660eb21db2a368c2e7b95
ArushiMishra25 <arushimishra2510@gmail.com>
2025-03-18 15:33:13 +0530

    cors changes

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ import imageRouter from "./routes/imageRoutes.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
-app.use(
+/*app.use(
   cors({
     origin: [
       "https://imagify-omega-gilt.vercel.app",
@@ -20,7 +20,7 @@ app.use(
     methods: "GET,POST,PUT,DELETE", // Allowed request types
     credentials: true, // If using cookies/sessions
   })
-);
+);*/
 
 await connectDB();
 
